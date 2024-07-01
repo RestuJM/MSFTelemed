dfbb5c3783275a97a58589a25918c79eb9cfa99d
RestuJM <53510437+RestuJM@users.noreply.github.com>
2024-07-01 15:34:48 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <title>Rainbow SDK for Web</title>
+        <title>Rainbow SDK for Web wowwowowwowo</title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
